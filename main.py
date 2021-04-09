3b79a280a3aae2f62ca3445869a9a8f88cb939e3
Samara David <samara.david@outlook.com>
2021-04-09 03:08:32 -0300

    cortada algumas partes do codigo que estavam redundantes

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class Jogador(object):
         logging.info("funcao iniciada")
         print(self.nome)
         sucesso = False
+
         while not sucesso:
             if self.tipo == MANUAL:
                 linha = int(input("Linha>")) - 1
@@ -24,7 +25,14 @@ class Jogador(object):
             elif self.tipo == ALEATORIO:
                 linha = random.randint(0,2)
                 coluna = random.randint(0,2)
+
             sucesso = tabuleiro.preencher(self.codigo, linha, coluna)
+            
+            if not sucesso and self.tipo == MANUAL:
+                print("ja jogou nesta posicao")
+            elif not sucesso:
+                logging.info("maquina jogou em uma posição ocupada, tentando de novo")
+        
         logging.info("jogada finalizada....")
         tabuleiro.imprimir()
         if tabuleiro.checar_vitoria():
@@ -49,84 +57,87 @@ class Tabuleiro(object):
             self.casas[linha][coluna] = jogador
             return True
         else:
-            print("ja jogou nesta posicao")
             return False
 
     def checar_velha(self):
         logging.info("função iniciada")
         zeros = []
+
         for linha in self.casas:
             if 0 in linha:
                 zeros.append(True)
             else:
                 zeros.append(False)
+
         logging.debug("lista de zeros" + str(zeros))
+
         if True not in zeros:
             print("FIM DE JOGO - DEU VELHA")
             return True
         else:
             return False
 
+    # checa uma unica linha
     def checar_linha(self, linha):
         if 0 in linha or not(linha[0] == linha[1] == linha[2]):
             return False
         else:
             return True
 
-    def checar_linhas(self, matriz):
+    def checar_matriz(self, matriz):
         for linha in matriz:
             if self.checar_linha(linha):
                 return True
 
+    # converte as 3 colunas para linhas juntas em uma matriz 3x3 
     def checar_colunas(self):
-        # kkkk piada
         logging.info("funcao iniciada")
         lista_aux = [[0,0,0], [0,0,0], [0,0,0]]
+
         for i in range(0,3):
             for j in range(3):
                 lista_aux[j][i] = self.casas[i][j]
-        return self.checar_linhas(lista_aux)
+        return self.checar_matriz(lista_aux)
 
-    def checar_diagonal(self):
+    # converte as diagonais em linhas 
+    def checar_diagonais(self):
         logging.info("funcao iniciada")
         lista_aux = [0,0,0]
-        resultados = []
+        matriz = []
+
         for i in range(3):
             lista_aux[i] = self.casas[i][i]
-        resultados.append(self.checar_linha(lista_aux))
+
+        matriz.append(lista_aux)
+
         logging.debug("diagonal1=" + str(lista_aux))
-        logging.debug("resultados=" + str(resultados))
-
+        logging.debug("estado da matriz: " + str(matriz))
         logging.info("processando segunda diagonal")
-        lista_aux = [0, 0, 0]
 
+        lista_aux = [0, 0, 0]
         j = 0
+
         for i in range(2, -1, -1):
-            logging.debug("j =" + str(j))
+            logging.debug("j=" + str(j))
             lista_aux[j] = self.casas[i][j]
             logging.debug("i=" + str(i))
             logging.debug("valor=" + str(lista_aux[j]))
             logging.debug("listaaux dentro do loop=" + str(lista_aux))
             j += 1
-        resultados.append(self.checar_linha(lista_aux))
-        logging.debug("diagonal2=" + str(lista_aux))
-        logging.debug("resultados=" + str(resultados))
 
-        if True in resultados:
+        matriz.append(lista_aux)
+        logging.debug("diagonal2=" + str(lista_aux))
+        logging.debug("estado da matriz" + str(matriz))
+
+        return self.checar_matriz(matriz)
+
+    def checar_vitoria(self):
+        logging.info("funcao iniciada ----------------")
+
+        if self.checar_matriz(self.casas) or self.checar_colunas() or self.checar_diagonais():
             return True
         else:
             return False
-
-    def checar_vitoria(self):
-        logging.info("funcao iniciada")
-        for linha in self.casas:
-            logging.debug("checando linha " + str(linha) + "--------")
-            if self.checar_linhas(self.casas):
-                return True
-            elif self.checar_colunas():
-                return True
-            elif self.checar_diagonal():
-                return self.checar_diagonal()
 
     def imprimir(self):
         for linha in self.casas:
@@ -136,26 +147,32 @@ class Main(object):
     def __init__(self):
         super().__init__()
         self.tabuleiro = Tabuleiro()
-        self.jogador1 = Jogador(1, "JOGADOR 1", 0)
-        self.jogador2 = Jogador(-1, "MAQUINA", 1)
+        self.jogador1 = Jogador(1, "JOGADOR 1", MANUAL)
+        self.jogador2 = Jogador(-1, "MAQUINA", ALEATORIO)
         self.fim = False
 
     def mainloop(self):
         self.tabuleiro.imprimir()
         while not self.fim: 
             self.fim = self.jogador1.jogar(self.tabuleiro)
+
             if self.fim:
                 break
             else:
                 logging.debug("jogador1 não ganhou, checando velha...")
                 self.fim = self.tabuleiro.checar_velha()
+
                 if self.fim:
                     break
                 else:
                     logging.debug("vez do jogador 2...")
                     self.fim = self.jogador2.jogar(self.tabuleiro)
-                    logging.debug("ninguem ganhou, procurando velha!")
-                    self.fim = self.tabuleiro.checar_velha()
+
+                    if self.fim:
+                        break
+                    else:
+                        logging.debug("ninguem ganhou, procurando velha!")
+                        self.fim = self.tabuleiro.checar_velha()
 
 random.seed()
 main = Main()
